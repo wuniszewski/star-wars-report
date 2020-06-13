a6431bf0f444dee23f6480eda57a0955213ce10d
Wojciech Uniszewski <uniszewski@gmail.com>
2020-06-13 18:17:32 +0200

    Initial commit

---
+++ src/main/java/pl/wuniszewski/starwarsreport/StarWarsReportApplication.java
@@ -0,0 +1,13 @@
+package pl.wuniszewski.starwarsreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wuniszewski/starwarsreport/StarWarsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wuniszewski.starwarsreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
