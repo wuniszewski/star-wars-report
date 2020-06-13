5ed84cadc4c27fc99fd418024f90c27227fc258e
Wojciech Uniszewski <uniszewski@gmail.com>
2020-06-13 21:46:26 +0200

    Included modelMapper dependency and added Bean for modelMapper

---
+++ src/main/java/pl/wuniszewski/starwarsreport/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package pl.wuniszewski.starwarsreport.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
