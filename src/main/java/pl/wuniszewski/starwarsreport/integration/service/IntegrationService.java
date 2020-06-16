70582a7cd86f39978925d3c040f9f76f39d8c9eb
Wojciech Uniszewski <uniszewski@gmail.com>
2020-06-16 21:18:52 +0200

    Added RestClientException handling

--- src/main/java/pl/wuniszewski/starwarsreport/integration/service/IntegrationService.java
+++ src/main/java/pl/wuniszewski/starwarsreport/integration/service/IntegrationService.java
@@ -3,6 +3,7 @@ package pl.wuniszewski.starwarsreport.integration.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import pl.wuniszewski.starwarsreport.integration.dto.CharacterDto;
 import pl.wuniszewski.starwarsreport.integration.dto.FilmDto;
@@ -28,17 +29,28 @@ public class IntegrationService {
     public List<PlanetDto> getPlanetsByName(String planetName) throws ResourceNotFoundException {
         return getPlanetSearchListOutcome(planetName).getResults();
     }
-    private PlanetSearchListOutcomeDto getPlanetSearchListOutcome (String planetName) throws ResourceNotFoundException {
-        PlanetSearchListOutcomeDto planetSearch = restTemplate.getForObject(urlPlanetSearchPrefix + planetName,
-                PlanetSearchListOutcomeDto.class);
-        if (planetSearch == null | planetSearch.getResults() == null) {
+
+    private PlanetSearchListOutcomeDto getPlanetSearchListOutcome(String planetName) throws ResourceNotFoundException {
+        PlanetSearchListOutcomeDto planetSearch;
+        try {
+            planetSearch = restTemplate.getForObject(urlPlanetSearchPrefix + planetName,
+                    PlanetSearchListOutcomeDto.class);
+        } catch (RestClientException e) {
+            planetSearch = new PlanetSearchListOutcomeDto();
+        }
+        if (planetSearch == null || planetSearch.getResults() == null) {
             throw new ResourceNotFoundException();
         }
         return planetSearch;
     }
 
     public CharacterDto getCharacterByEndpoint(String url) throws ResourceNotFoundException {
-        CharacterDto character = restTemplate.getForObject(url, CharacterDto.class);
+        CharacterDto character;
+        try {
+            character = restTemplate.getForObject(url, CharacterDto.class);
+        } catch (RestClientException e) {
+            character = new CharacterDto();
+        }
         if (isCharacterNullOrEmpty(character)) {
             throw new ResourceNotFoundException();
         }
@@ -46,11 +58,16 @@ public class IntegrationService {
     }
 
     private boolean isCharacterNullOrEmpty(CharacterDto character) {
-        return character == null | character.getFilms() == null | character.getName() == null | character.getUrl() == null;
+        return character == null || character.getFilms() == null || character.getName() == null | character.getUrl() == null;
     }
 
-    public FilmDto getFilmsByEndpoint(String url) throws ResourceNotFoundException {
-        FilmDto film = restTemplate.getForObject(url, FilmDto.class);
+    public FilmDto getFilmByEndpoint(String url) throws ResourceNotFoundException {
+        FilmDto film;
+        try {
+            film = restTemplate.getForObject(url, FilmDto.class);
+        } catch (RestClientException e) {
+            film = new FilmDto();
+        }
         if (isFilmNullOrEmpty(film)) {
             throw new ResourceNotFoundException();
         }
@@ -58,7 +75,7 @@ public class IntegrationService {
     }
 
     private boolean isFilmNullOrEmpty(FilmDto film) {
-        return film == null | film.getTitle() == null | film.getUrl() == null;
+        return film == null || film.getTitle() == null || film.getUrl() == null;
     }
 
 
